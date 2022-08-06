ea1178a434979e09ed7905d0743d4c4876dbb583
sanntiagorodriguez <santiagonicolas680@gmail.com>
2022-08-06 18:02:24 -0300

    Tercer desafio

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+let name = prompt("Ingrese su nombre:")
+let apellido = prompt("Ingrese su apellido:")
+let edad = prompt("Ingrese su edad:")
+
+alert("Bienvenid@" + name + " " + apellido + ", tu edad es " +edad  )
+let array = [];
+
+for( let i=1;i<=50;i++){
+	array.push(i);
+}
+console.log(array);
